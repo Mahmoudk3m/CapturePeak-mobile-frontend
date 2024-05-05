769fb9c5fa42ed0d4db481f378ed5b8508738072
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2024-05-05 21:39:37 +0300

    fix: nativewind bugs

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [['module:react-native-dotenv']],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,41 +1,9 @@
-import {AxiosError} from 'axios';
-import {
-  QueryClient,
-  UseQueryOptions,
-  UseMutationOptions,
-  DefaultOptions,
-} from 'react-query';
-import {PromiseValue} from 'type-fest';
+import {QueryClient} from '@tanstack/react-query';
 
-const queryConfig: DefaultOptions = {
-  queries: {
-    useErrorBoundary: true,
-    refetchOnWindowFocus: false,
-    retry: false,
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
   },
-};
-
-export const queryClient = new QueryClient({defaultOptions: queryConfig});
-
-export type ExtractFnReturnType<
-  FnType extends (...args: Parameters<FnType>) => ReturnType<FnType>,
-> = PromiseValue<ReturnType<FnType>>;
-
-export type QueryConfig<
-  QueryFnType extends (
-    ...args: Parameters<QueryFnType>
-  ) => ReturnType<QueryFnType>,
-> = Omit<
-  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
-  'queryKey' | 'queryFn'
->;
-
-export type MutationConfig<
-  MutationFnType extends (
-    ...args: Parameters<MutationFnType>
-  ) => ReturnType<MutationFnType>,
-> = UseMutationOptions<
-  ExtractFnReturnType<MutationFnType>,
-  AxiosError,
-  Parameters<MutationFnType> extends readonly [infer P1] ? P1 : never
->;
+});
